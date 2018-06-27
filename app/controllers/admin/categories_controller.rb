d780bf799a6eb8deccaeea3ed733d72dbeeb3d6e
tsang <B10423013@gemail.yuntech.edu.tw>
2018-06-27 18:24:44 +0800

    add form on index page for creating new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,5 +4,24 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      flash.now[:alert] = "restaurant was failed to create"
+      render :index
+    end
+  end
+
+  private 
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
