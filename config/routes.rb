58271bc4824d89dbcd585fe8f2dbbdaa3eaaa85f
tsang <tsangyuwen@gmail.com>
2018-07-03 21:46:16 +0800

    set users route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :comments, only:[:create, :destroy]
   end
   resources :categories, only: :show
+  resources :users, only: [:show, :edit, :update]
   root "restaurants#index"
 
   namespace :admin do 
