82b242ad9b4e9925bfe153b2c389160cf38142d0
tsang <tsangyuwen@gmail.com>
2018-07-06 12:20:24 +0800

    add feeds view show newest restaurant and comments

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, PhotoUploader
 	validates_presence_of :name
   	belongs_to :category
-  	has_many :comments
+  	has_many :comments, dependent: :destory
 end
