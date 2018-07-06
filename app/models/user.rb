d65849a2db06fa2e33cc367bb86dd9ef3785fa73
tsang <tsangyuwen@gmail.com>
2018-07-06 12:57:35 +0800

    add dependency between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	mount_uploader :image, PhotoUploader
-  has_many :comments
+  has_many :comments ,denpendent: :restrict_with_error
 	has_many :restaurants, through: :comments
 	validates_presence_of :name
   
