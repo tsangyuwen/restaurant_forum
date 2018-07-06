9efc515d4ec7654141c0d385ec821e070ab392bd
tsang <tsangyuwen@gmail.com>
2018-07-06 10:45:41 +0800

    setup routes for feed dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show] do
+    collection do
+      get :feeds
+    end
+    
+    member do
+      get :dashboard
+    end
+
     resources :comments, only:[:create, :destroy]
   end
   resources :categories, only: :show
